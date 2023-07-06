3aa372c41d2fa5fee2036c01ced00dfe95e8922c
Andrew Beveridge <andrew.beveridge@transferwise.com>
2023-07-06 01:26:09 -0500

    Improved caching workflow for repeated execution on same file

--- karaoke_generator/generator.py
+++ karaoke_generator/generator.py
@@ -1,6 +1,9 @@
 import os
+import json
+import urllib
 import shutil
 import logging
+import subprocess
 import yt_dlp
 import slugify
 from audio_separator import Separator
@@ -43,7 +46,19 @@ class KaraokeGenerator:
 
         if audio_file is None and youtube_url is None:
             raise Exception("Either audio_file or youtube_url must be specified as the input source")
+        if audio_file is not None and youtube_url is not None:
+            raise Exception("Only one of audio_file or youtube_url may be specified as the input source")
 
+        if audio_file is not None:
+            self.input_source_slug = slugify.slugify(os.path.basename(audio_file), lowercase=False)
+        if youtube_url is not None:
+            parsed_url = urllib.parse.urlparse(youtube_url)
+            self.input_source_slug = slugify.slugify(parsed_url.hostname + "-" + parsed_url.query, lowercase=False)
+
+        if self.output_dir is None:
+            self.output_dir = os.path.join(os.getcwd(), "karaoke-generator-output-" + self.input_source_slug)
+
+        self.output_filename_slug = None
         self.youtube_url = youtube_url
         self.youtube_video_file = None
         self.youtube_video_image_file = None
@@ -108,63 +123,126 @@ class KaraokeGenerator:
         if self.audio_file is None or not os.path.isfile(self.audio_file):
             raise Exception("Error: Invalid audio source provided.")
 
-        self.logger.debug(f"audio_file is valid file: {self.audio_file}, instantiating Separator")
+        self.logger.debug(f"audio_file is valid file: {self.audio_file}")
 
-        separator = Separator(
-            self.audio_file,
-            model_name=self.model_name,
-            model_file_dir=self.model_file_dir,
-            output_dir=self.cache_dir,
-        )
-        self.primary_stem_path, self.secondary_stem_path = separator.separate()
+        self.primary_stem_path = os.path.join(self.output_dir, f"{self.output_filename_slug}_(Instrumental)_{self.model_name}.wav")
+        self.secondary_stem_path = os.path.join(self.output_dir, f"{self.output_filename_slug}_(Vocals)_{self.model_name}.wav")
 
-        self.logger.debug(f"Separation complete! Output files: {self.primary_stem_path} {self.secondary_stem_path}")
+        if os.path.isfile(self.primary_stem_path) and os.path.isfile(self.secondary_stem_path):
+            self.logger.debug(f"Separated audio files already exist in output paths, skipping separation: {self.primary_stem_path}")
+        else:
+            self.logger.debug(f"instantiating Separator with model_name: {self.model_name} and output_dir: {self.output_dir}")
+            separator = Separator(
+                self.audio_file,
+                model_name=self.model_name,
+                model_file_dir=self.model_file_dir,
+                output_dir=self.output_dir,
+            )
+            self.primary_stem_path, self.secondary_stem_path = separator.separate()
+
+            self.logger.debug(f"Separation complete! Output files: {self.primary_stem_path} {self.secondary_stem_path}")
 
         self.output_values["primary_stem_path"] = self.primary_stem_path
         self.output_values["secondary_stem_path"] = self.secondary_stem_path
 
-        if self.output_dir:
-            shutil.copy(self.primary_stem_path, self.output_dir)
-            shutil.copy(self.secondary_stem_path, self.output_dir)
-
     def download_youtube_video(self):
-        # Options for downloading the original highest quality file to the cache dir
-        ydl_opts = {
-            "logger": YoutubeDLLogger(self.logger),
-            "format": "best",
-            "outtmpl": os.path.join(self.cache_dir, "%(id)s-%(title)s.%(ext)s"),
-        }
+        # Cache file path for the ydl.extract_info output
+        ydl_info_cache_file = os.path.join(self.cache_dir, f"ydl-info-{self.input_source_slug}.json")
 
-        # Download the original highest quality file
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(self.youtube_url, download=False)
-            temp_download_filepath = ydl.prepare_filename(info)
-            temp_download_parts = os.path.splitext(temp_download_filepath)
-            self.logger.debug(f"temp_download_filepath: {temp_download_filepath}")
+        # Check if the info cache file exists, if so, read from it
+        if os.path.isfile(ydl_info_cache_file):
+            self.logger.debug(f"Reading ydl info from cache: {ydl_info_cache_file}")
+            with open(ydl_info_cache_file, "r") as cache_file:
+                info = json.load(cache_file)
+                self.youtube_video_file = info["download_filepath"]
+                self.output_filename_slug = info["output_filename_slug"]
+        else:
+            self.logger.debug(f"No existing YDL info file found")
 
-            # Create a slugified download filepath to get rid of spaces and unicode characters from youtube titles,
-            # but retain original file extension (which may vary depending on the video format youtube returns)
-            temp_download_filename_parts = os.path.splitext(os.path.basename(temp_download_filepath))
-            filename_slug = slugify.slugify(temp_download_filename_parts[0])
+        if self.youtube_video_file is not None and os.path.isfile(self.youtube_video_file):
+            self.logger.debug(f"Existing file found at self.youtube_video_file, skipping download: {self.youtube_video_file}")
+        else:
+            self.logger.debug(f"No existing file found, preparing download")
+            # Options for downloading the original highest quality file to the cache dir
+            ydl_opts = {
+                "logger": YoutubeDLLogger(self.logger),
+                "format": "best",
+                "outtmpl": os.path.join(self.cache_dir, "%(id)s-%(title)s.%(ext)s"),
+            }
 
-            download_filepath = os.path.join(self.cache_dir, filename_slug + temp_download_parts[1])
-            self.logger.debug(f"download_filepath: {download_filepath}")
+            # Download the original highest quality file
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(self.youtube_url, download=False)
 
-            if os.path.isfile(download_filepath):
-                self.logger.debug(f"found existing file at download_filepath, skipping download: {download_filepath}")
-            else:
+                temp_download_filepath = ydl.prepare_filename(info)
+                self.logger.debug(f"temp_download_filepath: {temp_download_filepath}")
+
+                download_file_extension = os.path.splitext(os.path.basename(temp_download_filepath))[1]
+                self.logger.debug(f"download_file_extension: {download_file_extension}")
+
+                # Create a slugified filename prefix to get rid of spaces and unicode characters from youtube titles
+                info["output_filename_slug"] = info["id"] + "-" + slugify.slugify(info["title"], lowercase=False)
+                self.output_filename_slug = info["output_filename_slug"]
+                self.logger.debug(f"output_filename_slug: {self.output_filename_slug}")
+
+                # but retain original file extension (which may vary depending on the video format youtube returns)
+                info["download_filepath"] = os.path.join(self.cache_dir, self.output_filename_slug + download_file_extension)
+                self.logger.debug(f"download_filepath: {info['download_filepath'] }")
+
+                # Save the ydl.extract_info output to cache file
+                self.logger.debug(f"Saving sanitized YT-DLP info to cache: {ydl_info_cache_file}")
+                with open(ydl_info_cache_file, "w") as cache_file:
+                    json.dump(ydl.sanitize_info(info), cache_file, indent=4)
+
                 ydl.download([self.youtube_url])
-                shutil.move(temp_download_filepath, download_filepath)
-                self.youtube_video_file = download_filepath
+                shutil.move(temp_download_filepath, info["download_filepath"])
+                self.youtube_video_file = info["download_filepath"]
                 self.logger.debug(f"successfully downloaded youtube video to path: {self.youtube_video_file}")
 
-        self.audio_file = os.path.join(self.cache_dir, filename_slug + ".wav")
-        # TODO: extract audio from the downloaded video in WAV format using ffmpeg, writing to the filepath self.audio_file
-        self.logger.debug(f"self.audio_file updated: {self.audio_file}")
+        # Extract audio to WAV file using ffmpeg
+        self.audio_file = os.path.join(self.cache_dir, self.output_filename_slug + ".wav")
+        if os.path.isfile(self.audio_file):
+            self.logger.debug(f"Existing file found at self.audio_file, skipping extraction: {self.audio_file}")
+        else:
+            self.logger.debug(f"Extracting audio from {self.youtube_video_file} to {self.audio_file}")
+            subprocess.run(
+                [
+                    "ffmpeg",
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    self.youtube_video_file,  # Input file path
+                    "-vn",  # Only keep the audio
+                    "-f",
+                    "wav",  # Output format
+                    self.audio_file,  # Output file path
+                ]
+            )
+            self.logger.debug(f"Audio extracted to self.audio_file filepath: {self.audio_file}")
 
-        self.youtube_video_image_file = os.path.join(self.cache_dir, filename_slug + ".png")
-        # TODO: extract a still image from 30 seconds into the downloaded video in PNG format using ffmpeg, writing to image_filepath
-        self.logger.debug(f"self.youtube_video_image_file updated: {self.youtube_video_image_file}")
+        # Extract a still image from 30 seconds into the video using ffmpeg
+        self.youtube_video_image_file = os.path.join(self.cache_dir, self.output_filename_slug + ".png")
+        if os.path.isfile(self.youtube_video_image_file):
+            self.logger.debug(f"Existing file found at self.youtube_video_image_file, skipping extraction: {self.youtube_video_image_file}")
+        else:
+            self.logger.debug(f"Extracting still image from {self.youtube_video_file} to {self.youtube_video_image_file }")
+            subprocess.run(
+                [
+                    "ffmpeg",
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-ss",
+                    "30",  # Position to start extracting (30 seconds)
+                    "-i",
+                    self.youtube_video_file,  # Input file path
+                    "-vframes",
+                    "1",  # Only extract 1 frame (which will be a still image)
+                    self.youtube_video_image_file,  # Output file path
+                ]
+            )
+            self.logger.debug(f"Still image extracted to self.youtube_video_image_file filepath: {self.youtube_video_image_file}")
 
         self.output_values["youtube_video_file"] = self.youtube_video_file
         self.output_values["youtube_video_image_file"] = self.youtube_video_image_file
